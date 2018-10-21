9b64beade5abdebc7d8a015e93bb5196fe78c063
Francisco Vargas <fjvarg4934@gmail.com>
2018-10-21 11:52:45 -0500

    init commit

---
+++ models.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {firstName: String, lastName: String, required: true},
+  created: {type: Number}
+});
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}.`.trim();
+});
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName,
+    created: this.created || Date.now();
+  };
+};
