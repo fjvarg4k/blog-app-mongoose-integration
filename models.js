eb4786f0352480e723cbfe1d23e0a07bf9a1124b
Francisco Vargas <fjvarg4934@gmail.com>
2018-10-21 15:41:18 -0500

    Added author endpoints

--- models.js
+++ models.js
@@ -1,15 +1,40 @@
 "use strict";
 
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-const blogPostSchema = mongoose.Schema({
-  title: {type: String, required: true},
-  content: {type: String, required: true},
-  author: {
-    firstName: String,
-    lastName: String,
-  },
+var authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+    type: 'string',
+    unique: true
+  }
+});
+
+var commentSchema = mongoose.Schema({ content: 'string' });
+
+var blogPostSchema = mongoose.Schema({
+  title: 'string',
+  content: 'string',
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+  comments: [commentSchema],
   created: {type: Date, default: Date.now}
+});
+
+blogPostSchema.pre('find', function(next) {
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.pre('findOne', function(next) {
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.pre('findById', function(next) {
+  this.populate('author');
+  next();
 });
 
 blogPostSchema.virtual('authorName').get(function() {
@@ -22,9 +47,11 @@ blogPostSchema.methods.serialize = function() {
     title: this.title,
     content: this.content,
     author: this.authorName,
+    comments: this.comments,
     created: this.created
   };
 };
 
+var Author = mongoose.model('Author', authorSchema);
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
-module.exports = { BlogPost };
+module.exports = { Author, BlogPost };
