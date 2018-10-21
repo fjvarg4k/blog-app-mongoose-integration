deb8e2e805a007e6b1bc6410f5cbf91c0ee4ca4e
Francisco Vargas <fjvarg4934@gmail.com>
2018-10-21 12:38:03 -0500

    Fixed syntax error in server

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ function closeServer() {
 }
 
 if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err);)
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { app, runServer, closeServer };
